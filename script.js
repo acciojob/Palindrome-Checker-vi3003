d1d1e897d2b429ac0d9ebe2b31decdf167488239
vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-01-15 23:29:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+    str=str.toLowerCase();
+    strArr=str.split("");
+    strArr.reverse();
+    let strRev=strArr.join(''); 
+    if(str===strRev) return true;
+    return false;
 }
+
+
 module.exports = palindrome
